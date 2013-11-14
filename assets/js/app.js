a5f77c126a68386e9445035caf1264124d550e4b
Shana Goldman <shana.goldman@akqa.com>
2013-11-14 15:21:57 -0500

    set initial templates and app

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,67 @@
-var App = new Backbone.Marionette.Application();
+var BikeDataManager = new Backbone.Marionette.Application();
 
 // regions correspond to different areas of the application
-App.addRegions({
-	info: '#info'
+BikeDataManager.addRegions({
+	mainRegion: '#info'
+});
+
+ Bike = Backbone.Model.extend({
+	defaults: {
+		date: "",
+		distance: "",
+		time: "",
+		avgSpeed: "",
+		maxSpeed: "",
+		avgCadence: "",
+		location: ""
+	}
+});
+
+ Bikes = Backbone.Collection.extend({
+	model: Bike
+});
+
+ BikeView = Backbone.Marionette.ItemView.extend({
+	template: '#ride-template',
+	tagName: 'dl' 
+});
+
+BikeListView = Backbone.Marionette.CompositeView.extend({
+	template: '#rides-template',
+	itemView: BikeView,
+
+	itemViewContainer: '.ride-info'
+});
+
+BikeDataManager.addInitializer(function(options) {
+	var bikeListView = new BikeListView({
+		collection: options
+	});
+
+	BikeDataManager.mainRegion.show(bikeListView);
 });
 
 $(document).ready(function() {
-	App.start();
+	var options = [
+			{
+				"date": "5/14",
+				"distance": "10.53",
+				"time": "52:45",
+				"avgSpeed": "11.98",
+				"maxSpeed": "18.34",
+				"avgCadence": "51",
+				"location": ""
+			},
+			{
+				"date": "5/25",
+				"distance": "29.29",
+				"time": "2:29:54",
+				"avgSpeed": "11.72",
+				"maxSpeed": "24.25",
+				"avgCadence": "55",
+				"location": "Riley's Lock"			
+			}];
+
+	var bikes = new Bikes(options);
+	BikeDataManager.start(bikes);
 });
