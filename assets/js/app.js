8a3e72a6521a0af75a5bee5af91772f6a5ba3771
Shana Goldman <shana.goldman@akqa.com>
2013-11-14 17:13:55 -0500

    display multiple bike data as a collection

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-var BikeDataManager = new Backbone.Marionette.Application();
+var BikeDataManager = new Marionette.Application();
 
 //set up main region on the page
 BikeDataManager.addRegions({
@@ -18,28 +18,50 @@ BikeDataManager.Bike = Backbone.Model.extend({
 	}
 });
 
+//set the collection with the Bike model
+BikeDataManager.BikeCollection = Backbone.Collection.extend({
+	model: BikeDataManager.Bike
+});
+
 //set the model's view. (the definition list)
-BikeDataManager.BikeView = Backbone.Marionette.ItemView.extend({
+BikeDataManager.BikeItemView = Marionette.ItemView.extend({
 	template: '#ride-template',
 	tagName: 'dl'
 });
 
+//set the collection's view.
+BikeDataManager.BikesView = Marionette.CollectionView.extend({
+	itemView: BikeDataManager.BikeItemView
+});
+
 //initialize the page.
 BikeDataManager.addInitializer(function(){
-	var bike = new BikeDataManager.Bike({
-		"date": "5/14",
-		"distance": "10.53",
-		"time": "52:45",
-		"avgSpeed": "11.98",
-		"maxSpeed": "18.34",
-		"avgCadence": "51",
-		"location": ""
-	});
+	//set a collection of data.
+	var bikes = new BikeDataManager.BikeCollection([
+			{
+			"date": "5/14",
+			"distance": "10.53",
+			"time": "52:45",
+			"avgSpeed": "11.98",
+			"maxSpeed": "18.34",
+			"avgCadence": "51",
+			"location": ""
+		},
+		{
+			"date": "5/25",
+			"distance": "29.29",
+			"time": "2:29:54",
+			"avgSpeed": "11.72",
+			"maxSpeed": "24.25",
+			"avgCadence": "55",
+			"location": "Riley's Lock"			
+		}
+	]);
 
-	var bikeView = new BikeDataManager.BikeView({
-		model: bike
+	var bikesView = new BikeDataManager.BikesView({
+		collection: bikes //pass a collection to the BikesView -- which is a collection view.
 	});
 
 	//show the main region of the app
-	BikeDataManager.mainRegion.show(bikeView);
+	BikeDataManager.mainRegion.show(bikesView);
 });
